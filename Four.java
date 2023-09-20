d5a1cbb14ab535b5572794fbfbfdf8532e1e91d7
Gaeun-Kim <0107162a@gmail.com>
2023-09-20 11:04:06 +0900

    four

---
+++ Four.java
@@ -0,0 +1,8 @@
+package a201914114;
+
+public class Four {
+
+    public static void print() {
+        System.out.println("four");
+    }
+}
--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ package a201914114;
 public class Main {
 
     public static void main(String[] args) {
+        Two.print();
         System.out.println("one 201914114");
         Three.print();
-
+        Four.print();
     }
 }
